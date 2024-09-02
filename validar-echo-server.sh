c625220e482c0830cfe2c2cd6e7251ed02627412
Lucas Aldazabal <lucas@aldazabal.com.ar>
2024-09-02 17:34:15 -0300

    add netcat timeout

--- validar-echo-server.sh
+++ validar-echo-server.sh
@@ -1,5 +1,5 @@
 docker run --network tp0_testing_net -it alpine sh -c '
- RESPONSE=$(echo "ping" | nc server:12345) 
+ RESPONSE=$(echo "ping" | nc server:12345 -w 3) 
  if [ "$RESPONSE" == "ping" ]; then
    echo "action: test_echo_server | result: success"
  else
